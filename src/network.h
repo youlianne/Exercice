e5743738c2d2e06f72a5aac264ba6627a51f2461
Julian Raub <julianraub@tsf-444-wpa-7-120.epfl.ch>
2018-11-07 14:40:38 +0100

    ajout fonctionalite set values

--- src/network.h
+++ src/network.h
@@ -27,17 +27,15 @@ public:
   where *degree(n)* is Poisson-distributed.
 
   All previous links are erased first.
-  @param[in] mean_deg the average of the Poisson distribution,
-  @param[out] number of links created.
+  @param mean_deg the average of the Poisson distribution.
  */
     size_t random_connect(const double&);
 /*! Resets node values with a vector of *n* new values.
   If the current size is *s* and *n<s* then only the first *n* values are changed.
   If *n>s* then only *s* values are used. 
   The internal \ref values vector keeps the same size.
-
-  @param[in] vector of new node values,
-  @param[out] number of nodes succesfully reset.
+  @param[in] vector of new node values
+  @param[out] number of nodes succesfully reset
  */
     size_t set_values(const std::vector<double>&);
 /*! Number of nodes */
